c9060680c088d8ffc8156fe72335792c471129f4
abdoulahi21 <abdoualhi.diallo@unchk.edu.sn>
2024-03-08 13:04:48 +0000

    integration de la base de donnee

---
+++ src/main/java/com/example/projet/model/BD.java
@@ -0,0 +1,27 @@
+package com.example.projet.model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BD {
+    private final String server="localhost";
+    private final String username="root";
+    private final String password="root";
+    private final String bd="projet";
+    private final String url=""+"jdbc:mysql://"+server+":3306/"+bd;
+    private Connection conn;
+    public Connection getConnection()
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn=  DriverManager.getConnection(url,username,password);
+            System.out.println("Connecté");
+        }catch (Exception ex)
+        {
+            conn=null;
+            System.out.println("Erreur");
+        }
+        return conn;
+    }
+
+
+}
